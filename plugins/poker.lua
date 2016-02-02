c988be6e339ac49164faa01de4d5044325f238e7
yanarsin <lfrshydfr@gmail.com>
2016-02-02 08:42:16 +0000

    Create poker.lua

---
+++ plugins/poker.lua
@@ -0,0 +1,18 @@
+do
+
+function run(msg, matches)
+  return " 😐 "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^😐",
+    "^😐😐",
+    "^😐😐😐",
+   "^😐😐😐😐",
+    "^>😐😐😐😐😐",
+  },
+  run = run
+}
+end
