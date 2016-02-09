5f1a43deb5215e7d998fe1c061ef38b57cce2e74
yanarsin <lfrshydfr@gmail.com>
2016-02-09 14:58:16 +0000

    Create rastin.lua

---
+++ plugins/rastin.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return " چیه؟؟چیکارش داری؟ "
+  end
+return {
+  description = "poker!", 
+  usage = "fuck : ",
+  patterns = {
+    "^rastin",
+  },
+  run = run
+}
+end
